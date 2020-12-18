fa61e53b7b794dd37aaada1f6f263518ba2b7454
ERFANIUM <officialerfanshield@gmail.com>
2020-12-18 16:40:54 +0330

    rc

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ import fs from "fs";
 import path from "path";
 import fastify from "fastify";
 import Database from "better-sqlite3";
-import { prepareDatabase } from "./src/prepareDatabase.js";
 import randomString from "crypto-random-string";
 
-const dbDir = process.env.DB_DIR || '.';
-if (!fs.existsSync(dbDir)) fs.mkdirSync(dbDir);
+const DB_DIR = process.env.DB_DIR || ".";
+if (!fs.existsSync(DB_DIR)) fs.mkdirSync(DB_DIR);
 
 const PORT = process.env.PORT || 3030;
+const REDIRECT_URL = process.env.REDIRECT_URL || "https://github.com/erfanium/simple-url-shortner";
 
 const app = fastify({ logger: false });
-const db = new Database(path.join(dbDir, "app.db"));
-prepareDatabase(db);
+const db = new Database(path.join(DB_DIR, "app.db"));
+
+// Prepare db
+db.exec(`CREATE TABLE IF NOT EXISTS links(
+  id VARCHAR PRIMARY KEY,
+  url VARCHAR
+)`);
+
+// ROOT REDIRECT
+app.get("/", (_, reply) => reply.redirect(301, REDIRECT_URL));
 
 // COUNT LINKS
 const count = db.prepare("SELECT COUNT(id) FROM links");
@@ -51,9 +59,7 @@ app.route({
 });
 
 app.addHook("onError", (request, reply, error, done) => {
-  !error.statusCode || reply.statusCode >= 500
-    ? console.error(error)
-    : console.log(error);
+  !error.statusCode || reply.statusCode >= 500 ? console.error(error) : console.log(error);
   done();
 });
 
--- src/prepareDatabase.js
+++
@@ -1,6 +0,0 @@
-export function prepareDatabase(db) {
-   db.exec(`CREATE TABLE IF NOT EXISTS links(
-      id VARCHAR PRIMARY KEY,
-      url VARCHAR
-   )`);
-}
