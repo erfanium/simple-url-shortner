78e3f1b0041aa4565f4864ec3507294f0164f2bf
ERFANIUM <officialerfanshield@gmail.com>
2020-12-18 16:02:17 +0330

    0.0.1

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+import fs from "fs";
+import path from "path";
 import fastify from "fastify";
 import Database from "better-sqlite3";
 import { prepareDatabase } from "./src/prepareDatabase.js";
 import randomString from "crypto-random-string";
 
+const dbDir = process.env.DB_DIR || '.';
+if (!fs.existsSync(dbDir)) fs.mkdirSync(dbDir);
+
 const PORT = process.env.PORT || 3030;
-const BASE_URL = process.env.BASE_URL;
 
-if (!BASE_URL) throw new Error("BASE_URL env-var is required");
-
-const app = fastify();
-const db = new Database("app.db");
+const app = fastify({ logger: false });
+const db = new Database(path.join(dbDir, "app.db"));
 prepareDatabase(db);
 
+// COUNT LINKS
+const count = db.prepare("SELECT COUNT(id) FROM links");
+app.get("/count", () => count.get()["COUNT(id)"]);
+
 /// GET
-const getById = db.prepare("SELECT * from links WHERE id = ?");
+const getById = db.prepare("SELECT * FROM links WHERE id = ?");
 app.get("/:id", (req, reply) => {
   const result = getById.get(req.params.id);
   if (!result) return reply.status(404).send();
@@ -29,7 +35,11 @@ app.route({
   url: "/",
   schema: {
     body: {
-      url: { type: "string" },
+      type: "object",
+      required: ["url"],
+      properties: {
+        url: { type: "string" },
+      },
     },
   },
   async handler(req) {
@@ -38,6 +48,13 @@ app.route({
     create.run(params);
     return params;
   },
+});
+
+app.addHook("onError", (request, reply, error, done) => {
+  !error.statusCode || reply.statusCode >= 500
+    ? console.error(error)
+    : console.log(error);
+  done();
 });
 
 app.listen(PORT, "0.0.0.0").then(() => console.log("On", PORT));
